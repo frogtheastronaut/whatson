74b5181f8d209e74f2c0eddb7abbd72f87c7ea24
Ethan Zhang <ethanzhangyixuan@gmail.com>
2025-10-03 11:59:48 +1000

    Added false positives removal

--- n3t5/sites.py
+++ n3t5/sites.py
@@ -1,19 +1,18 @@
 import json
 import os
 
-
-
-
 class SitesInformation:
     def __init__(self, data_file_path=None):
         if not data_file_path:
             data_file_path = os.path.join(os.path.dirname(__file__), "resources", "data.json")
+            false_positives_file_path = os.path.join(os.path.dirname(__file__), "resources", "false_positives.txt")
         
         with open(data_file_path, "r", encoding="utf-8") as file:
             site_data = json.load(file)
-        
-        site_data.pop('$schema', None)
+        # remove all false positives from site_data
+        with open(false_positives_file_path, "r", encoding="utf-8") as fp_file:
+            false_positives = [line.strip() for line in fp_file]
+        for site in false_positives:
+            if site in site_data:
+                del site_data[site]
         self.sites = site_data
-
-
-
